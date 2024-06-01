fca2eef10a5a1ef2faaa6bea50ddd0c2bc05d0b4
yamasaki <kochisk8@gmail.com>
2024-06-01 19:06:31 +0900

    勤退管理

--- src/routes/web.php
+++ src/routes/web.php
@@ -19,6 +19,7 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/', [TimeCardController::class, 'index'])->name('home');
 
 Route::get('/records', [TimeCardController::class, 'records'])->name('records');
